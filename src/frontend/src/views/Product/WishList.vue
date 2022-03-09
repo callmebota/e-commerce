60e4d346f000b8ccf819eee39d19ff16cffd16b3
callmebota <callmebotacallmebota1102@gmail.com>
2022-03-10 01:09:13 +0600

    Initial commit

---
+++ src/frontend/src/views/Product/WishList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="pt-3">Your WishList</h4>
+      </div>
+    </div>
+
+<!--   display products-->
+    <div class="row">
+      <div v-for="product of products" :key="product.id"
+           class="col-md-6 col-xl-4 col-12 pt-3 justify-content-around d-flex">
+        <product-box :product="product"></product-box>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import ProductBox from "../../components/ProductBox";
+
+export default {
+  components: {ProductBox},
+  data() {
+    return {
+      token: null,
+      products: null,
+    };
+  },
+  props: ["baseURL"],
+  methods: {
+    fetchWishList() {
+      axios.get(`${this.baseURL}wishlist/${this.token}`).then((data) => {
+        this.products = data.data;
+      }).catch((err) => {
+        console.log("err", err);
+      });
+    },
+  },
+  mounted() {
+    this.token = localStorage.getItem("token");
+    this.fetchWishList();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main/java/com/example/FullStackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackJavaApplication.class, args);
+    }
+
+}
